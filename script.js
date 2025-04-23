1a19edb020001305722556edd3f3b5ec8656bcb6
kunalbhardwaj882-code <kunalbhardwaj882@gmail.com>
2025-04-23 15:53:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.login-btn').addEventListener('click', function() {
+    alert('Login form will pop up.');
+});
+
+document.querySelector('.signup-btn').addEventListener('click', function() {
+    alert('Sign-up form will pop up.');
+});
